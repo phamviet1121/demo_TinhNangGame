1e5a10279dfe1b9eb625217625bfa5653eda25b9
phamviet11211 <phamviet4454@gmail.com>
2024-08-13 16:20:17 +0700

    chinh code

--- Assets/_Assets/a_luu_tru/code/nhiemvu/QuestManager.cs
+++ Assets/_Assets/a_luu_tru/code/nhiemvu/QuestManager.cs
@@ -14,61 +14,64 @@ public class QuestManager : MonoBehaviour
     //[SerializeField] List<QuestDataa> listData; 
     void Start()
     {
-        // load();
-        a = s.GetChild(0).gameObject;
+        load();
+        //    a = s.GetChild(0).gameObject;
 
-        //int len = listData.Count;
-        foreach (var data in questDatabase_1.DatabaseList)
-        {
-            b = Instantiate(a, s);
-            b.transform.GetChild(0).GetComponent<Image>().sprite = data.anh_img;
-            b.transform.GetChild(1).GetComponent<Text>().text = data.thongtin_txt;
-            b.transform.GetChild(2).GetComponent<Text>().text = data.tiendo_txt;
-            b.transform.GetChild(3).GetComponent<Button>().interactable = data.trangthai_bl;
+        //    //int len = listData.Count;
+        //    foreach (var data in questDatabase_1.DatabaseList)
+        //    {
+        //        b = Instantiate(a, s);
+        //        b.transform.GetChild(0).GetComponent<Image>().sprite = data.anh_img;
+        //        b.transform.GetChild(1).GetComponent<Text>().text = data.thongtin_txt;
+        //        b.transform.GetChild(2).GetComponent<Text>().text = data.tiendo_txt;
+        //        b.transform.GetChild(3).GetComponent<Button>().interactable = data.trangthai_bl;
+        //    }
+        //    Destroy(a);
         }
-        Destroy(a);
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        void load()
+        {
+            a = s.GetChild(0).gameObject;
+
+            //int len = listData.Count;
+            foreach (var data in questDatabase_1.DatabaseList)
+            {
+                b = Instantiate(a, s);
+                b.transform.GetChild(0).GetComponent<Image>().sprite = data.anh_img;
+                b.transform.GetChild(1).GetComponent<Text>().text = data.thongtin_txt;
+                b.transform.GetChild(2).GetComponent<Text>().text = data.tiendo_txt;
+                b.transform.GetChild(3).GetComponent<Button>().interactable = data.trangthai_bl;
+            }
+
+            //int len = listData.Count;
+            //Debug.Log($"{len}");
+            //for (int i = 0; i < len; i++)
+            //{
+            //    b = Instantiate(a, s);
+
+            //    Debug.Log($"{b}");
+
+            //    b.transform.GetChild(0).GetComponent<Image>().sprite = listData[i].anh_img;
+            //    b.transform.GetChild(1).GetComponent<Text>().text = listData[i].thongtin_txt;
+
+            //    Debug.Log($"{b.transform.GetChild(1).GetComponent<Text>().text}");
+
+            //    b.transform.GetChild(2).GetComponent<Text>().text = listData[i].tiendo_txt;
+            //    b.transform.GetChild(3).GetComponent<Button>().interactable = listData[i].trangthai_bl;
+            //}
+
+
+
+            Destroy(a);
+
+
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    //void load()
-    //{
-    //    a = s.GetChild(0).gameObject;
-
-    //    //int len = listData.Count;
-    //    foreach (var data in questDatabase_1.DatabaseList)
-    //    {
-    //        b = Instantiate(a, s);
-    //        b.transform.GetChild(0).GetComponent<Image>().sprite = data.anh_img;
-    //        b.transform.GetChild(1).GetComponent<Text>().text = data.thongtin_txt;
-    //        b.transform.GetChild(2).GetComponent<Text>().text = data.tiendo_txt;
-    //        b.transform.GetChild(3).GetComponent<Button>().interactable = data.trangthai_bl;
-    //    }
-
-        //int len = listData.Count;
-        //Debug.Log($"{len}");
-        //for (int i = 0; i < len; i++)
-        //{
-        //    b = Instantiate(a, s);
-
-        //    Debug.Log($"{b}");
-
-        //    b.transform.GetChild(0).GetComponent<Image>().sprite = listData[i].anh_img;
-        //    b.transform.GetChild(1).GetComponent<Text>().text = listData[i].thongtin_txt;
-
-        //    Debug.Log($"{b.transform.GetChild(1).GetComponent<Text>().text}");
-
-        //    b.transform.GetChild(2).GetComponent<Text>().text = listData[i].tiendo_txt;
-        //    b.transform.GetChild(3).GetComponent<Button>().interactable = listData[i].trangthai_bl;
-        //}
-
-
-
-       // Destroy(a);
-
-       
-    //}
-}
